67400739d38287822d6656820f4ff480f1438d9a
limingxuan-pku <2000012801@stu.pku.edu.cn>
2022-12-02 06:08:10 -0800

    second commit

--- build/src/crt0-vexriscv.d
+++
@@ -1,2 +0,0 @@
-src/crt0-vexriscv.o: src/crt0-vexriscv.S \
- /home/limx/CFU-Playground/soc/build/digilent_arty.my_kws/software/include/generated/soc.h
--- build/src/tensorflow/lite/kernels/internal/portable_tensor_utils.d
+++
@@ -1,5 +0,0 @@
-src/tensorflow/lite/kernels/internal/portable_tensor_utils.o: \
- src/tensorflow/lite/kernels/internal/portable_tensor_utils.cc \
- src/tensorflow/lite/kernels/internal/portable_tensor_utils.h \
- src/tensorflow/lite/c/builtin_op_data.h src/tensorflow/lite/c/common.h \
- src/tensorflow/lite/c/c_api_types.h
